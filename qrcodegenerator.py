161b57856fc61844d2615b0902a976e835bb3236
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-12-21 08:01:38 +0000

    Final Commit

--- qrcodegenerator.py
+++ qrcodegenerator.py
@@ -1,5 +1,18 @@
 import qrcode
 import image
 
-qr = qrcode.QRCode
+qr = qrcode.QRCode (
+    version = 15, 
+     box_size = 10,
+     border = 5 # It is the white part of the image --border in all 4 sides with white color
 
+)
+
+data = "https://github.com/komlalex"
+
+qr.add_data(data)
+
+qr.make(fit = True)
+
+img = qr.make_image(fill = "black", back_color = "white")
+img.save("komla_qr_code.png")
